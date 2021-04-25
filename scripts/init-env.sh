b3fee584a64d5442c4d26efc49fd60e267bbe1e1
The Kingdom Of God Is Within You <The_Kingdom_Of_God_Is_Within_You@protonmail.com>
2021-04-25 22:18:23 +0100

    :construction: Travis test 1.

--- scripts/init-env.sh
+++ scripts/init-env.sh
@@ -1,16 +1,9 @@
-#!/bin/bash -x
-
-init-env(){
-    python -m venv .venv
-    . .venv/bin/activate
-    # reactivate with zsh 
-    # https://stackoverflow.com/questions/45216663/how-to-automatically-activate-virtualenvs-when-cding-into-a-directory
-    current_dir=$(basename "$PWD")
-    cd ..
-    cd $current_dir
-    python -m pip install --upgrade pip
-    pip install wheel
-    pip install pip-tools
-    pip-sync ./requirements/requirements.txt
-    pip install -e .[dev,test,docs]
-}
+#!/bin/bash
+SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
+python -m venv .venv
+. $SCRIPT_PATH/../.venv/bin/activate  # Use this to activate the virtual environment
+python -m pip install --upgrade pip
+pip install wheel
+pip install pip-tools
+$SCRIPT_PATH/pip-sync.sh
+pip install -e .[dev,test,docs]
